6a7ebfa0a0ed98d056401b9ac8d27f1da7780f45
BrenoFreitasM <brenofreitasmendes@gmail.com>
2025-05-17 17:41:20 -0300

    feat: scrollview inside scrollview

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import { ScrollView, Text } from "react-native";
 
 export default function App() {
   return (
-    <ScrollView horizontal>
+    <ScrollView >
       <Text
         style={{ marginTop: 300, fontSize: 32 }}
       >
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium voluptates aperiam architecto, porro ea dolore saepe et. Provident molestias fugit obcaecati soluta non, at laborum magnam tempora doloribus, velit dolorum!
       </Text>
+
+      <ScrollView horizontal>
+      <Text
+        style={{ fontSize: 32 }}
+      >
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium voluptates aperiam architecto, porro ea dolore saepe et. Provident molestias fugit obcaecati soluta non, at laborum magnam tempora doloribus, velit dolorum!
+      </Text>
+    </ScrollView>
     </ScrollView>
   )
 }
