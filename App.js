607a5993a2046e244a878114472bbc6de73b43a7
BrenoFreitasM <brenofreitasmendes@gmail.com>
2025-05-17 17:12:23 -0300

    feat: using ellipsizeMode

--- App.js
+++ App.js
@@ -2,14 +2,15 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={{ width: 200}}>
+    <View >
       <Text
-        style={{ marginTop: 300, fontSize: 32, width: 100 }}
+        style={{ marginTop: 300, fontSize: 32 }}
         selectable
         selectionColor="#f00"
         numberOfLines={1}
+        ellipsizeMode="tail"
       >
-        Lorem ipsum dolor sit amet consectetur adipisicing .
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium voluptates aperiam architecto, porro ea dolore saepe et. Provident molestias fugit obcaecati soluta non, at laborum magnam tempora doloribus, velit dolorum!
       </Text>
     </View>
   )
