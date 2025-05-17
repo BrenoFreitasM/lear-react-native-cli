4b95b9f8f96dfdaa5077fae293cd3655a1b07035
BrenoFreitasM <brenofreitasmendes@gmail.com>
2025-05-17 17:39:38 -0300

    feat: horizontal scroll

--- App.js
+++ App.js
@@ -2,14 +2,10 @@ import { ScrollView, Text } from "react-native";
 
 export default function App() {
   return (
-    <ScrollView >
+    <ScrollView horizontal>
       <Text
         style={{ marginTop: 300, fontSize: 32 }}
       >
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium voluptates aperiam architecto, porro ea dolore saepe et. Provident molestias fugit obcaecati soluta non, at laborum magnam tempora doloribus, velit dolorum!
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium voluptates aperiam architecto, porro ea dolore saepe et. Provident molestias fugit obcaecati soluta non, at laborum magnam tempora doloribus, velit dolorum!
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium voluptates aperiam architecto, porro ea dolore saepe et. Provident molestias fugit obcaecati soluta non, at laborum magnam tempora doloribus, velit dolorum!
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium voluptates aperiam architecto, porro ea dolore saepe et. Provident molestias fugit obcaecati soluta non, at laborum magnam tempora doloribus, velit dolorum!
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium voluptates aperiam architecto, porro ea dolore saepe et. Provident molestias fugit obcaecati soluta non, at laborum magnam tempora doloribus, velit dolorum!
       </Text>
     </ScrollView>
